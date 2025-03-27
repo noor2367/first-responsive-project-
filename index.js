d188585be0f90b264b3d3bb995b6da4132f8b3fd
noor2367 <ebdevelopernoor8@gmail.com>
2025-03-27 12:10:19 -0700

    add close btn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const menu = document.querySelector(".nav-menu")
 const hamburger = document.querySelector(".hamburger")
+const closeBtn = document.querySelector(".close-btn")
+
 
 hamburger.addEventListener("click", function(){
-    menu.classList.toggle("active")
+    menu.classList.add("active")
+    hamburger.style.display ="none"
+})
+
+closeBtn.addEventListener("click", function() {
+    menu.classList.remove("active")
+    hamburger.style.display ="block"
 })
